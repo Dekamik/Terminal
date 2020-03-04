375080d282351d0230e47d45754048a479bb2ab0
Dennis von Bargen <dennis.vonbargen@soprasteria.com>
2020-03-04 16:07:03 +0100

    Neutralized design

--- src/components/clock/Clock.js
+++ src/components/clock/Clock.js
@@ -8,7 +8,7 @@ class Clock extends React.Component {
     }
 
     componentDidMount() {
-        this.interval = setInterval(() => this.setState({ time: new Date() }), 250);
+        this.interval = setInterval(() => this.setState({ time: new Date() }), 1000);
     }
 
     componentWillUnmount() {
@@ -17,10 +17,10 @@ class Clock extends React.Component {
 
     render() {
         return (
-        <div>
-            <h1 className="time">{this.state.time.toLocaleTimeString("sv-se")}</h1>
+        <span className="clock">
+            <h1 className="time">{this.state.time.toLocaleTimeString("sv-se", {hour: "2-digit", minute: "2-digit"})}</h1>
             <h3 className="date">{Date.dayName(this.state.time.getDay(), true)} den {this.state.time.getDate()}{Date.dateSuffix(this.state.time.getDate())} {Date.monthName(this.state.time.getMonth())} {this.state.time.getFullYear()}</h3>
-        </div>
+        </span>
         );
     }
 }
